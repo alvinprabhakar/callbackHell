0fb8034fba55436c90c9e78f9d16c89e687b6b1a
Alvin Prabha <alvinprabhakar@gmail.com>
2021-08-17 19:15:35 +0530

    added index

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let countdown;
+let  countdown_number = 10;
+
+
+window.onload = setTimeout(() => {
+    document.body.innerText = "10"
+    setTimeout(() => {
+    document.body.innerText = "9"
+    setTimeout(() => {
+    document.body.innerText = "8"
+    setTimeout(() => {
+    document.body.innerText = "7"
+    setTimeout(() => {
+    document.body.innerText = "6"
+    setTimeout(() => {
+    document.body.innerText = "5"
+    setTimeout(() => {
+    document.body.innerText = "4"
+    setTimeout(() => {
+    document.body.innerText = "3"
+    setTimeout(() => {
+    document.body.innerText = "2"
+    setTimeout(() => {
+    document.body.innerText = "1"
+    setTimeout(()=> {document.body.innerText = "Happy New Year"
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    }, 1000)
+    },1000);
+
+
+    
